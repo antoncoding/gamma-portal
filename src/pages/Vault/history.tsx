102f16c461748c715546134be5e64a32c24c4d7c
Anton Cheng <antonassocareer@gmail.com>
2020-10-14 21:18:33 +0800

    hide warnings

--- src/pages/Vault/history.tsx
+++ src/pages/Vault/history.tsx
@@ -1,57 +1,58 @@
-import React, { useContext, useMemo, useState, useCallback } from 'react'
-import { useParams } from 'react-router-dom';
+import React from 'react'
+// import React, { useContext, useMemo, useState, useCallback } from 'react'
+// import { useParams } from 'react-router-dom';
 
-import { DataView, useToast, Timer } from '@aragon/ui'
-import BigNumber from 'bignumber.js'
+// import { DataView, useToast, Timer } from '@aragon/ui'
+// import BigNumber from 'bignumber.js'
 
-import { walletContext } from '../../contexts/wallet'
+// import { walletContext } from '../../contexts/wallet'
 
-import { getVaultHistory } from '../../utils/graph'
+// import { getVaultHistory } from '../../utils/graph'
 import SectionTitle from '../../components/SectionHeader'
-import CustomIdentityBadge from '../../components/CustomIdentityBadge'
+// import CustomIdentityBadge from '../../components/CustomIdentityBadge'
 
-import {DepositCollateralBadge, WithdrawCollateralBadge} from '../../components/ActionBadge'
+// import {DepositCollateralBadge, WithdrawCollateralBadge} from '../../components/ActionBadge'
 
-import useAsyncMemo from '../../hooks/useAsyncMemo';
+// import useAsyncMemo from '../../hooks/useAsyncMemo';
 
 export default function VaultHistory() {
 
-  const [isLoading, setIsLoading] = useState(true)
+  // const [isLoading, setIsLoading] = useState(true)
 
-  const { web3, networkId, user } = useContext(walletContext)
-  const { owner, vaultId } = useParams()
-  const toast = useToast()
+  // const { web3, networkId, user } = useContext(walletContext)
+  // const { owner, vaultId } = useParams()
+  // const toast = useToast()
 
-  const history = useAsyncMemo(async () => {
-    const { burnShortActions, depositCollateralActions, depositLongActions, mintShortActions, withdrawCollateralActions, withdrawLongActions } = await getVaultHistory(networkId, owner, vaultId, toast)
-    const allEntry = [...burnShortActions, ...depositCollateralActions, ...depositLongActions, ...mintShortActions, ...withdrawCollateralActions, ...withdrawLongActions]
-      .sort((a, b) => a.timestamp >= b.timestamp ? -1 : 1)
-    setIsLoading(false)
-    return allEntry
-  }, [], [])
+  // const history = useAsyncMemo(async () => {
+  //   const { burnShortActions, depositCollateralActions, depositLongActions, mintShortActions, withdrawCollateralActions, withdrawLongActions } = await getVaultHistory(networkId, owner, vaultId, toast)
+  //   const allEntry = [...burnShortActions, ...depositCollateralActions, ...depositLongActions, ...mintShortActions, ...withdrawCollateralActions, ...withdrawLongActions]
+  //     .sort((a, b) => a.timestamp >= b.timestamp ? -1 : 1)
+  //   setIsLoading(false)
+  //   return allEntry
+  // }, [], [])
 
-  console.log(`history`, history)
-
-  const renderRow = useCallback((entry: {oToken: undefined | {id: string, symbol:string }, asset:string, amount:string}) => {
-    const badge = <WithdrawCollateralBadge />
-    const asset = entry.oToken
-      ? <CustomIdentityBadge entity={entry.oToken.id} label={entry.oToken.symbol} /> 
-      : <CustomIdentityBadge entity={entry.asset} label="now" />
+  // const renderRow = useCallback((entry: {oToken: undefined | {id: string, symbol:string, decimals: number }, asset:{id: string, symbol:string, decimals: number}, amount:string}) => {
+  //   const badge = <WithdrawCollateralBadge />
+  //   const asset = entry.oToken
+  //     ? <CustomIdentityBadge entity={entry.oToken.id} label={entry.oToken.symbol} /> 
+  //     : <CustomIdentityBadge entity={entry.asset.id} label={entry.asset.symbol} />
     
-    return [
-      badge,
+  //   return [
+  //     badge,
+  //     asset,
 
-    ]
-  }, [])
+  //   ]
+  // }, [])
 
   return (
     <>
-
       <SectionTitle title={'History'} />
       {/* <DataView
+        entries={history}
         mode="table"
         status={isLoading ? 'loading' : 'default'}
         fields={['type', 'asset', 'amount', 'timestamp']}
+        renderEntry={renderRow}
       /> */}
     </>
   )
