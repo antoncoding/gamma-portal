1b8a2de98a404efa5735490e98b06152cdd66818
Anton Cheng <antonassocareer@gmail.com>
2020-12-11 17:15:13 +0800

    Merge conflicts

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -24,7 +24,6 @@ export const tokens: TokensTyps = {
       decimals: 6,
     },
   ],
-<<<<<<< HEAD
   // '4' : [
   //   emptyToken,
   //   {
@@ -47,30 +46,6 @@ export const tokens: TokensTyps = {
   //   }
   // ],
   '42' : [
-=======
-  '4': [
-    emptyToken,
-    {
-      name: 'USDC',
-      address: '0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b',
-      symbol: 'USDC',
-      decimals: 6,
-    },
-    {
-      name: 'Wrapped Ether',
-      address: '0xc778417e063141139fce010982780140aa0cd5ab',
-      symbol: 'WETH',
-      decimals: 18,
-    },
-    {
-      name: 'Wrapped BTC',
-      address: '0x577d296678535e4903d59a4c929b718e1d575e0a',
-      symbol: 'WBTC',
-      decimals: 8,
-    },
-  ],
-  '42': [
->>>>>>> c53abf7... Add operator info to operators page
     emptyToken,
     {
       name: 'USDC',
@@ -103,7 +78,6 @@ export const addressese: GammaAddress = {
     addressBook: '',
     pool: '',
   },
-<<<<<<< HEAD
   // '4': {
   //   controller: '0x5ca72b05416f728c877942078454d458e6733421',
   //   factory: '0x17fFbe9476dDcC79f4574E0c89CE537fD65A8cAe',
@@ -123,38 +97,11 @@ export const blacklistOTokens = {
   // '4': ["0x080022f16f3ad1d29c42a64ab2449fda25ffafc0", "0x1b3e291570f1c35013a252b8c52fef9f3dc46783", "0x4824cb60348702718db00ebbfb78a364b71094ef", "0xcbdbbbca5cc390d3a2a0f71177d2ea3d0e7d6e1c", "0xff7094938a547b96cdfc9582644e1413421f9efc"],
   '42': [ZERO_ADDR]
 }
-=======
-  '4': {
-    controller: '0x5ca72b05416f728c877942078454d458e6733421',
-    factory: '0x17fFbe9476dDcC79f4574E0c89CE537fD65A8cAe',
-    addressBook: '0x7630e7dE53E3d1f298f653d27fcF3710c602331C',
-    pool: '0x228D386D950984D1F4A2425683e620558A1430d9',
-  },
-  '42': {
-    controller: '0x162114ca77be3234e459c816be93c3a32c3ad5a7',
-    factory: '0x7EaEbc642Af4E92245105De1958B87BF8ee89757',
-    addressBook: '0xD37752Fd2976335fddb2e6A2cF5FfbfA88bf5F05',
-    pool: '0x48ab3c5ff91cce46cd96d0d31e935ca53b885b2e',
-  },
-};
-
-export const blacklistOTokens = {
-  '1': [ZERO_ADDR],
-  '4': [
-    '0x080022f16f3ad1d29c42a64ab2449fda25ffafc0',
-    '0x1b3e291570f1c35013a252b8c52fef9f3dc46783',
-    '0x4824cb60348702718db00ebbfb78a364b71094ef',
-    '0xcbdbbbca5cc390d3a2a0f71177d2ea3d0e7d6e1c',
-    '0xff7094938a547b96cdfc9582644e1413421f9efc',
-  ],
-  '42': [ZERO_ADDR],
-};
 
 export const knownOperators: {
   [key in SupportedNetworks]: { address: string; name: string; audited: boolean }[];
 } = {
   '1': [],
-  '4': [],
   '42': [
     {
       address: '0x3bee6b613b3e52020fa958ebd9ca691c11a55995',
@@ -163,4 +110,3 @@ export const knownOperators: {
     },
   ],
 };
->>>>>>> c53abf7... Add operator info to operators page
