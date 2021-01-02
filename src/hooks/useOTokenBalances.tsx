217f46a51e0ba16b6562c5b361ceb875d5d7f3bd
Anton Cheng <antonassocareer@gmail.com>
2021-01-03 00:45:12 +0800

    Update getOTokenBalance hook

--- src/hooks/useOTokenBalances.tsx
+++ src/hooks/useOTokenBalances.tsx
@@ -1,15 +1,17 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 
 import { useToast } from '@aragon/ui'
 import { OTokenBalance } from '../types'
 import { getBalances } from '../utils/graph'
-import useAsyncMemo from './useAsyncMemo'
 import { SupportedNetworks } from '../constants/networks'
 
 export function useOTokenBalances(
   account: string,
   networkId: SupportedNetworks,
+  refetchInterval?: number,
 ): { balances: OTokenBalance[] | null; refetch: Function; isLoading: boolean } {
+  const [balances, setBalances] = useState<OTokenBalance[]>([])
+
   const [refreshCount, setRefreshCount] = useState(0)
   const [isLoading, setIsLoadig] = useState(true)
   const toast = useToast()
@@ -18,15 +20,18 @@ export function useOTokenBalances(
     setRefreshCount(count => count + 1)
   }, [setRefreshCount])
 
-  const balances = useAsyncMemo(
-    async () => {
+  useEffect(() => {
+    async function updateBalances() {
       const balances = await getBalances(networkId, account, toast)
+      if (balances === null) return
       setIsLoadig(false)
-      return balances
-    },
-    null,
-    [refreshCount, account, networkId],
-  )
+      setBalances(balances)
+      setRefreshCount(count => count + 1)
+    }
+    updateBalances()
+    const interval = setInterval(updateBalances, refetchInterval ? refetchInterval * 1000 : 10000)
+    return clearInterval(interval)
+  }, [networkId, account, toast, refreshCount, refetchInterval])
 
   return { balances, isLoading, refetch }
 }
