6d32e7f310ba81f9e6d12333916d8a166e042686
Anton Cheng <antonassocareer@gmail.com>
2021-01-04 22:18:55 +0800

    Add payable proxy

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -94,7 +94,7 @@ export const knownOperators: {
   ],
   '42': [
     {
-      address: '0x3bee6b613b3e52020fa958ebd9ca691c11a55995',
+      address: '0x5957a413f5ac4bcf2ba7c5c461a944b548adb1a',
       name: 'PayableProxy',
       audited: true,
     },
