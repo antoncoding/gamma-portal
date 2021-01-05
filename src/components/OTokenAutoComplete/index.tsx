cc479350eef4a381f2b4a0ba4f12c8dafc2c635f
Anton Cheng <antonassocareer@gmail.com>
2021-01-05 20:29:44 +0800

    Improve OTokenBalance and OTokenAutoComplete components

---
+++ src/components/OTokenAutoComplete/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { _AutoComplete as AutoComplete } from '@aragon/ui'
+
+import { simplifyOTokenSymbol } from '../../utils/others'
+import { SubgraphOToken } from '../../types'
+
+type AutoCompleteProps = {
+  oTokens: SubgraphOToken[]
+  selectedOToken: SubgraphOToken | null
+  setSelectedOToken: React.Dispatch<React.SetStateAction<SubgraphOToken | null>>
+}
+
+export default function OTokenAutoComplete({ selectedOToken, setSelectedOToken, oTokens }: AutoCompleteProps) {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  return (
+    <AutoComplete
+      items={oTokens.filter((o: SubgraphOToken) => {
+        return (
+          searchTerm &&
+          (simplifyOTokenSymbol(o.symbol).toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 || o.id === searchTerm)
+        )
+      })}
+      renderItem={(o: SubgraphOToken) => simplifyOTokenSymbol(o.symbol)}
+      onChange={setSearchTerm}
+      onSelect={(o: SubgraphOToken) => {
+        setSearchTerm(simplifyOTokenSymbol(o.symbol))
+        setSelectedOToken(o)
+      }}
+      wide
+      value={searchTerm ? searchTerm : selectedOToken ? simplifyOTokenSymbol(selectedOToken?.symbol) : ''}
+      placeholder="Search 01JAN21, 800P, or address"
+    />
+  )
+}
