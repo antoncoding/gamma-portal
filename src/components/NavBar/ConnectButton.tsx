137c132676025a7287469029a9efdb7c1cedd8df
Anton Cheng <antonassocareer@gmail.com>
2021-01-18 00:30:43 +0800

    Add colored circle to indicate whether it's mainnet or testnet (#74)

--- src/components/NavBar/ConnectButton.tsx
+++ src/components/NavBar/ConnectButton.tsx
@@ -4,10 +4,12 @@ import { Button, IconConnect, Box, IconPower, IdentityBadge } from '@aragon/ui'
 
 import { checkAddressAndAddToStorage } from '../../utils/storage'
 import { useConnectedWallet } from '../../contexts/wallet'
+import { SupportedNetworks } from '../../constants'
 
 function ConnectButton() {
-  const { connect, disconnect, user } = useConnectedWallet()
+  const { networkId, connect, disconnect, user } = useConnectedWallet()
 
+  const color = networkId === SupportedNetworks.Mainnet ? 'rgb(3, 135, 137, 0.7)' : '#8F7FFE'
   const connectWeb3 = async () => {
     const address = await connect()
     if (!address) return
@@ -17,6 +19,17 @@ function ConnectButton() {
   return user !== '' ? (
     <>
       <Box padding={6}>
+        <div
+          style={{
+            verticalAlign: 'middle',
+            marginRight: '7px',
+            display: 'inline-block',
+            backgroundColor: color,
+            borderRadius: '50%',
+            width: '10px',
+            height: '10px',
+          }}
+        />
         <IdentityBadge
           entity={user}
           popoverAction={{
