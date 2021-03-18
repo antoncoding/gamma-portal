141590d3483a398638ec01d0a22876d15c498e5b
Anton Cheng <antonassocareer@gmail.com>
2021-03-18 16:04:06 +0800

    use env variable to control if using internal or public network

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -5,6 +5,8 @@ type TokensTyps = {
   [key in SupportedNetworks]: Token[]
 }
 
+const isPublic = process.env.REACT_APP_PUBLIC === 'true'
+console.log(`isPublic`, isPublic)
 export const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
 
 export const eth: Token = {
@@ -67,19 +69,19 @@ type SystemAddresses = {
 
 export const addresses: SystemAddresses = {
   '1': {
-    controller: '0x4ccc2339F87F6c59c6893E1A678c2266cA58dC72',
-    factory: '0x7C06792Af1632E77cb27a558Dc0885338F4Bdf8E',
-    addressBook: '0x1E31F2DCBad4dc572004Eae6355fB18F9615cBe4',
-    pool: '0x5934807cC0654d46755eBd2848840b616256C6Ef',
+    controller: isPublic ? '0x4ccc2339F87F6c59c6893E1A678c2266cA58dC72' : '0xde158Fa7022f11707d4a3570eec4621B35d83829',
+    factory: isPublic ? '0x7C06792Af1632E77cb27a558Dc0885338F4Bdf8E' : '0xE21127f47B365d3b1467746804f32BF8dCf47e26',
+    addressBook: isPublic ? '0x1E31F2DCBad4dc572004Eae6355fB18F9615cBe4' : '0x57ADe7D5E9D2F45A07f8039Da7228ACC305fbeaF',
+    pool: isPublic ? '0x5934807cC0654d46755eBd2848840b616256C6Ef' : '0x0Cb5BDBf1726f7CC720B21EA910ACeda9FdDf680',
     zeroxExchange: '0x61935cbdd02287b511119ddb11aeb42f1593b7ef',
     zeroxERCProxy: '0x95e6f48254609a6ee006f7d493c8e5fb97094cef',
     zeroxStaking: '0xa26e80e7dea86279c6d778d702cc413e6cffa777',
   },
   '42': {
-    controller: '0xdee7d0f8ccc0f7ac7e45af454e5e7ec1552e8e4e',
-    factory: '0xb9d17ab06e27f63d0fd75099d5874a194ee623e2',
-    addressBook: '0x8812f219f507e8cfe9d2f1e790164714c5e06a73',
-    pool: '0x8c7c60d766951c5c570bbb7065c993070061b795',
+    controller: isPublic ? '0xdee7d0f8ccc0f7ac7e45af454e5e7ec1552e8e4e' : '0xa84cff11957a0a08a3e1d568ed1caaf47626c1f3',
+    factory: isPublic ? '0xb9d17ab06e27f63d0fd75099d5874a194ee623e2' : '0x32b5a18238BAdF23F8E88669de2bD3671ff7BF83',
+    addressBook: isPublic ? '0x8812f219f507e8cfe9d2f1e790164714c5e06a73' : '0x4163Bf53878B2169Ea9E404b9E840FA010DbF949',
+    pool: isPublic ? '0x8c7c60d766951c5c570bbb7065c993070061b795' : '0xFf7a2BD21f6dAb62948Bb7545266E9a6b2a0bEb2',
     zeroxExchange: '0x4eacd0af335451709e1e7b570b8ea68edec8bc97',
     zeroxERCProxy: '0xf1ec01d6236d3cd881a0bf0130ea25fe4234003e',
     zeroxStaking: '0xbab9145f1d57cd4bb0c9aa2d1ece0a5b6e734d34',
@@ -104,7 +106,7 @@ export const knownOperators: {
 } = {
   '1': [
     {
-      address: '0x8f7dd610c457fc7cb26b0f9db4e77581f94f70ac',
+      address: isPublic ? '0x8f7dd610c457fc7cb26b0f9db4e77581f94f70ac' : '0xa05157b27b7db2eb63bb0c11412b71e7de027f89',
       name: 'PayableProxy',
       description: 'Proxy contract to help mint calls with ETH instead of WETH',
       audited: true,
@@ -113,7 +115,7 @@ export const knownOperators: {
   ],
   '42': [
     {
-      address: '0x5957a413f5ac4bcf2ba7c5c461a944b548adb1a5',
+      address: isPublic ? '0x5957a413f5ac4bcf2ba7c5c461a944b548adb1a5' : '0xe501e882f6e5f049899e02b7e48d89f223cb2a4f',
       name: 'PayableProxy',
       description: 'Proxy contract to help mint calls with ETH instead of WETH',
       audited: true,
