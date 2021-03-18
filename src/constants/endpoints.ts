4a3e399bf0449991941e09c99dad3b284fe5f747
Anton Cheng <antonassocareer@gmail.com>
2021-03-18 16:10:26 +0800

    use env variable to control if using internal or public network (#94)

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -4,9 +4,15 @@ type graphEndPointType = {
   [key in SupportedNetworks]: string
 }
 
+const isPublic = process.env.REACT_APP_PUBLIC === 'true'
+
 export const subgraph: graphEndPointType = {
-  '1': 'https://api.thegraph.com/subgraphs/name/opynfinance/gamma-mainnet',
-  '42': 'https://api.thegraph.com/subgraphs/name/opynfinance/gamma-kovan',
+  '1': isPublic
+    ? 'https://api.thegraph.com/subgraphs/name/opynfinance/gamma-mainnet'
+    : 'https://api.thegraph.com/subgraphs/name/opynfinance/playground',
+  '42': isPublic
+    ? 'https://api.thegraph.com/subgraphs/name/opynfinance/gamma-kovan'
+    : 'https://api.thegraph.com/subgraphs/name/opynfinance/gamma-internal-kovan',
 }
 
 export const ZeroXEndpoint: { [key in SupportedNetworks]: { http: string; ws: string } } = {
