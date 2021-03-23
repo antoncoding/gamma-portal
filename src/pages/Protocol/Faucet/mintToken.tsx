1ab572dfb85f33b199cdc6ab44c25082b28462db
Anton Cheng <antonassocareer@gmail.com>
2021-03-23 13:21:25 +0800

    Add greedy check

--- src/pages/Protocol/Faucet/mintToken.tsx
+++ src/pages/Protocol/Faucet/mintToken.tsx
@@ -3,7 +3,9 @@ import BigNumber from 'bignumber.js'
 import { TextInput, Button, useTheme, LoadingRing } from '@aragon/ui'
 
 import SectionHeader from '../../../components/SectionHeader'
+import Warning from '../../../components/Warning'
 import { useConnectedWallet } from '../../../contexts/wallet'
+
 import { Token } from '../../../types'
 import { fromTokenAmount, toTokenAmount } from '../../../utils/math'
 import { useNotify } from '../../../hooks/useNotify'
@@ -25,6 +27,8 @@ export default function MintToken({ token }: { token: Token }) {
 
   const tokenBalance = useTokenBalance(token.id, user, 10)
 
+  const isGreedy = useMemo(() => amount > 300000, [amount])
+
   const mint = useCallback(async () => {
     setIsMinting(true)
     try {
@@ -44,9 +48,10 @@ export default function MintToken({ token }: { token: Token }) {
       <div style={{ fontSize: 14, color: theme.contentSecondary }}>
         My Balance: {toTokenAmount(tokenBalance, token.decimals).toFormat(4)} {token.symbol}
       </div>
+      <Warning text="You're being greedy!" show={isGreedy} />
       <div style={{ display: 'flex' }}>
         <TextInput type="number" value={amount} onChange={e => setAmount(e.target.value)} />
-        <Button onClick={mint} disabled={isMinting}>
+        <Button onClick={mint} disabled={isMinting || isGreedy}>
           {' '}
           {isMinting ? <LoadingRing /> : 'Mint'}{' '}
         </Button>
